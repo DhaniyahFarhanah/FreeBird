248de31870f271d6348f36d6594039c291780fbc
DhaniyahFarhanah <noobygal21@gmail.com>
2024-03-21 16:21:14 +0800

    Sprites implemented

---
+++ Free Bird/Assets/Scripts/BgEffectScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgEffectScript : MonoBehaviour
+{
+
+    [SerializeField] float speedMove;
+    [SerializeField] Vector2 moveTowardsPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameStateManager.GetGameStatus() && !GameStateManager.GetEnd()) //Playing the game
+        {
+            transform.position = Vector2.MoveTowards(transform.position, moveTowardsPoint, speedMove * Time.deltaTime);
+        }
+            
+    }
+}
